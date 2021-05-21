d504b8a086777b4878effe2bf18907a11d02f292
Mohammad Haghighat <hosseinhg@hotmail.com>
2021-05-22 02:33:21 +0430

    Remove ImageSelector selected image OnClick a button

--- src/Shared/HandyControlDemo_Shared/UserControl/Controls/ImageSelectorDemoCtl.xaml.cs
+++ src/Shared/HandyControlDemo_Shared/UserControl/Controls/ImageSelectorDemoCtl.xaml.cs
@@ -1,10 +1,20 @@
-﻿namespace HandyControlDemo.UserControl
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace HandyControlDemo.UserControl
 {
     public partial class ImageSelectorDemoCtl
     {
         public ImageSelectorDemoCtl()
         {
             InitializeComponent();
+        }
+        void OnClick1(object sender, RoutedEventArgs e)
+        {
+            test.HasValue = false;
+            test.PreviewBrush = default(Brush);
+            test.Uri = default(Uri);
         }
     }
 }
